bb210219055a71cec452bd766f06ddb3d3ec413c
andis854 <123587604+andis854@users.noreply.github.com>
2023-01-26 21:28:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Teachers',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      
